6a004c6649183dc54c6c0b087c97d5d8b2b88750
Mayanks584 <ranamayank584@gmail.com>
2024-11-14 21:40:28 +0000

    conversion

---
+++ .devcontainer/01_basic/03_conversion.js
@@ -0,0 +1,22 @@
+//let score = undefined
+//console.log(typeof score)
+//console.log(typeof (score)); we also can write in this form and marzi hai semi column use karna ki nahi  
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+// NaN  == not a number
+// true  = 1 false = 0
+//"33" => 33 number
+// "33abc => nan"
+//let isLoggedIn = "1"
+//let booleanloggedIn = Boolean(isLoggedIn)
+//console.log(booleanloggedIn)
+let isLoggedIn = "8"
+let booleanloggedIn = Boolean(isLoggedIn)
+//console.log(booleanloggedIn)
+// in empty string false
+let number = 44
+let stringNumber = String(number)
+//console.log(stringNumber)
+console.log(typeof stringNumber)
+
