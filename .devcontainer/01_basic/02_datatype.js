399ccde20df8f7e973410465c6afeb1b24e5aa84
Mayanks584 <ranamayank584@gmail.com>
2024-11-14 14:45:31 +0000

    datatype

---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version matlab isko use karne se sara code new code ban jayega by engine
+// comment
+//alert(3 + 3) // we are using nodejs not browser this is work in console
+console.log(3+3)  // code readability should be high 
+// console.log(3+4); console.log("mayank") not do like this 
+let name = "mayank" //string
+let age = 34 // number
+let islogged = false // boolen
+// number => 2 to power 53
+// bigint use when the number is very arge
+// string => ""
+// boolean => true/false
+// null => stand alone value hai represention of empty value 
+// undefined => when the value is not given
+// symbol => use to find the unique 
+// object 
+console.log(typeof "mayank")
+console.log(typeof null)
+// null is type of object
+// undefined is undefined
