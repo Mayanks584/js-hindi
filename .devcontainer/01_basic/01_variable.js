6c1d3e7d73cabc6af04c58dbaa52ccd8b8021e82
Mayanks584 <ranamayank584@gmail.com>
2024-11-14 15:10:46 +0000

    variable

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 123334335 // in const we can not change the value 
+let accountEmail = "mayanfa@gmail.com" // in let we can change the value let and var is variable 
+var accountPassword = "1234" // same in var we can change the value 
+accountCity = "hp" // we can also declare the variable like this but not do like this
+//accountId = 2
+// {} this are scope 
+ 
+// never use var beause of issuse in block scope and   functional scope 
+/*  this is also comment */ 
+
+console.log(accountId); // use to print 
+accountEmail = "jafnjaf.com"
+accountPassword = "sdfsff"
+accountCity = "bihar"
+let accountstate;
+console.table([accountEmail,accountPassword,accountCity,accountstate]) // use to print multiple line 
