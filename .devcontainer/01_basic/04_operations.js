e7d3aa1d0e4fd0d4a98a2d48050d0bf964370483
Mayanks584 <ranamayank584@gmail.com>
2024-11-15 10:05:10 +0000

    operations

---
+++ .devcontainer/01_basic/04_operations.js
@@ -0,0 +1,26 @@
+let value = 33
+let negValue = -value
+//console.log(negValue)
+// ** means power me value 2**2 % remainder
+
+// let str1 = "hello mayank"
+// let str2 =" rana"
+// let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" +2 )
+// console.log(1+"2") // this is a problem or complex problem 
+// console.log("1" + "3")
+// console.log("1" +2 + 2) // if the string is at first then it will treat as string
+// console.log(1 +2 + "2") //32  if string at last then conversion is done
+// console.log(3+4 *4 % 3)
+console.log(+true); // simple likh  not possible true+
+console.log(+"")
+// assignmenet operator
+let num1 ,num2, num3
+num1 = num2 =num3 = 2+2
+console.log([num1,num2,num3])
+let gamecounter=200
+gamecounter++
+console.log(gamecounter)
+
+
