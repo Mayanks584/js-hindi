650ea169a024ed1647063dec71dc059508856234
Mayanks584 <ranamayank584@gmail.com>
2024-11-21 11:16:11 +0000

    comparision

---
+++ .devcontainer/01_basic/05_comparision.js
@@ -0,0 +1,17 @@
+//console.log(2>1)
+// != not equal to == equal to 
+// when the datatype is not same 
+// console.log("2">1);
+// console.log("02" >1);// data type should be same
+// console.log(null >0);
+console.log(null <0);
+console.log(null ==0); // eqaul to sign
+console.log(null >=0);
+//  comparison operation(>,<,=<,=>)convert null to zero or treat it as a zero 
+// equality work different 
+console.log(undefined ==0);
+console.log(undefined =>0); // this is function anonymous
+console.log(undefined >0);
+ // strick check  (===) check datatype also then do comparison if datatype is not same then it is false 
+console.log("2"===2);
+
