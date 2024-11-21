de9abc03592c8daa0eda5118a0aa5f1e5217997b
Mayanks584 <ranamayank584@gmail.com>
2024-11-21 11:16:23 +0000

    nothing

--- .devcontainer/01_basic/04_operations.js
+++ .devcontainer/01_basic/04_operations.js
@@ -11,8 +11,8 @@ let negValue = -value
 // console.log(1+"2") // this is a problem or complex problem 
 // console.log("1" + "3")
 // console.log("1" +2 + 2) // if the string is at first then it will treat as string
-// console.log(1 +2 + "2") //32  if string at last then conversion is done
-// console.log(3+4 *4 % 3)
+// console.log(1 +2 + "2") //32  if string at last then conversion is done of firsrt two
+// console.log(3+4 *4 % 3) 
 console.log(+true); // simple likh  not possible true+
 console.log(+"")
 // assignmenet operator
--- .devcontainer/01_basic/text.js
+++ .devcontainer/01_basic/text.js
@@ -1,1 +1,17 @@
-console.log("mayank") // display on the screen means to print the value
+// // console.log("mayank") // display on the screen means to print the value
+// const fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`Fruit ${i + 1}: ${fruits[i]}`);
+// }
+
+         let arr=[1,2,3,4];
+         for(let i=0; i<arr.lenght;i++){
+             console.log(arr[i]);
+         }
+
+        let ar=[1,2,3,4];
+        for(let value of ar){
+            console.log(value);
+        }
+    // fee ki class hai ignore karo isko 
